a41a1537012cee5a7b80e8f5f51cd35af7622b06
Edward Lonsdale <edward.lonsdale@digital.cabinet-office.gov.uk>
2026-02-26 11:24:29 +0000

    feat: remove unused log group

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -5,11 +5,3 @@ resource "aws_cloudwatch_log_group" "wagtail" {
     Name = "${local.task_name}-logs"
   }
 }
-
-resource "aws_cloudwatch_log_group" "smtp" {
-  name              = "/ecs/${local.task_name}-smtp"
-  retention_in_days = local.log_retention_days
-  tags = {
-    Name = "${local.task_name}-smtp"
-  }
-}
